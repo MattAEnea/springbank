bd9ece77526fd2574e3a72eab2f7af3a16bd4079
MattAEnea <matthew.a.enea@gmail.com>
2019-07-02 03:35:29 -0400

    added @Service annotation

--- src/main/java/com/bankapp/service/CustomerService.java
+++ src/main/java/com/bankapp/service/CustomerService.java
@@ -1,10 +1,12 @@
 package com.bankapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bankapp.beans.Customer;
 import com.bankapp.dao.CustomerRepo;
 
+@Service
 public class CustomerService 
 {
 	@Autowired
