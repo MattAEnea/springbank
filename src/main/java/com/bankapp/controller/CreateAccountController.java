9fabb50c5a859f4a970b8d4bd9e48e8761b88c7f
MattAEnea <matthew.a.enea@gmail.com>
2019-07-02 03:35:10 -0400

    added method to save object to database and free variable afterwards

--- src/main/java/com/bankapp/controller/CreateAccountController.java
+++ src/main/java/com/bankapp/controller/CreateAccountController.java
@@ -1,16 +1,21 @@
 package com.bankapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bankapp.beans.Customer;
+import com.bankapp.service.CustomerService;
 import com.bankapp.service.PasswordService;
 
 @Controller
 public class CreateAccountController 
 {
+	@Autowired
+	CustomerService customerService;
+	
 	@PostMapping("/save")
 	public String createAccount(@RequestParam String fullName, 
 			 @RequestParam String username, @RequestParam String password, 
@@ -24,7 +29,9 @@ public class CreateAccountController
 		customer.setPassword(PasswordService.hashThePass(password, salt));
 		salt = null;
 		password = null;
+		customerService.saveCustomer(customer);
 		model.addAttribute("newAccount", customer);
+		customer = null;
 		return "account";
 	}
 	
