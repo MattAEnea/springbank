ca3664a8d045aa9e2c112fb334b53ec1f646c140
MattAEnea <matthew.a.enea@gmail.com>
2019-07-19 01:06:35 -0400

    Added code to check if login exists

--- src/main/java/com/bankapp/controller/LoginController.java
+++ src/main/java/com/bankapp/controller/LoginController.java
@@ -1,5 +1,37 @@
 package com.bankapp.controller;
 
-public class LoginController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bankapp.beans.Customer;
+import com.bankapp.service.CustomerService;
+import com.bankapp.service.PasswordService;
+
+@Controller
+public class LoginController
+{
+	@Autowired
+	CustomerService customerService;
+	
+	@PostMapping("/loginProcess")
+	public String accountLogin(@RequestParam String username, 
+			@RequestParam String password, Model model)
+	{
+		Customer customer = customerService.findByUser(username);
+		if(PasswordService.checkPass(password, customer.getPassword(),
+				customer.getSalt()))
+		{
+			model.addAttribute("customerLogin", customer);
+			return "account";
+		}
+		else
+		{
+			model.addAttribute("msg", "wrong password");
+			return "err";
+		}
+	}
+	
 }
